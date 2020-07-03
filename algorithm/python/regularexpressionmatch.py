40339563c47c16e8b4b59609b326c831d419926d
Keunmo <9858momo@gmail.com>
2020-07-04 02:02:54 +0900

    today's prac

---
+++ algorithm/python/regularexpressionmatch.py
@@ -0,0 +1,14 @@
+import re
+def isMatch(s, p):
+    p = re.compile(p)
+    m = p.fullmatch(s)
+    print(m)
+    if m:
+        print(m)
+        return True
+    else:
+        print("False")
+        return False
+
+# isMatch('ab','.*')
+# isMatch("mississippi","mis*is*p*.")

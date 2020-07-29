4110c1447d08208e0393084da47f77507401bc2d
Keunmo <9858momo@gmail.com>
2020-07-30 00:13:16 +0900

    add annotation at prob 1028

--- algorithm/python/1028_recov_tree_from_preodr_trav.py
+++ algorithm/python/1028_recov_tree_from_preodr_trav.py
@@ -10,34 +10,34 @@ class Solution:
     def recoverFromPreorder(self, S: str) -> TreeNode:
         if len(S)==0:
             return None
-        lvlist=[]   # (lv,node) 형식의 튜플.
+        lvlist=[]   # list of tuple (level, node)
         lv = 1
         S=S.split('-')
         node = TreeNode(S[0])
         lvlist.append((0,node)) 
-        # lvlist 의 첫번째 요소를 반복문 밖에서 수동으로 추가하는 이유: 
-        # lv를 공백 합으로 계산하는데 S.split 하면 첫번째 lv0이랑 두번째 lv1의 공백이 둘 다 0이라서.
-        # 그래서 for문 안에서 두번째 요소부터 돌고 lv는 1부터 시작함.
+        # manually append 1st elem in lvlist because, 
+        # this code calc lv by count non-digit elem(==''), but when do S.split(), 1st elem(lv 0) and 2nd elem(lv 1)'s whitespace is same.
+        # so it start iter at 2nd elem, and lv starts at 1.
         for i in S[1:]:
             if i.isdigit():
                 node = TreeNode(i)
                 lvlist.append((lv,node))
                 lv=1
-            if not i.isdigit():
+            else:
                 lv+=1
         stk=[]
         stk.append(lvlist[0])
-        # 스택 첫번째 요소 밖에서 추가하는 이유는 for 안에서 스택의 top과 비교하는 작업을 해주기 때문.
+        # append 1st elem at stack manually because for loop compare elem with stk's top.
         for i in lvlist[1:]:
             if stk[-1][0] + 1 == i[0]:      #stk top lv+1 == i lv
-                stk[-1][1].left = i[1]      #stk top node.left = i node. 처음 만나는 자식노드니까 무조건 left.
+                stk[-1][1].left = i[1]      #stk top node.left = i node. bc it is 1st child.
                 stk.append(i)
             else:
-                while not stk[-1][0] + 1 == i[0]:   #stk top lv+1 이 i lv가 될때까지 pop. ==부모노드 나올때까지 pop.
+                while not stk[-1][0] + 1 == i[0]:   #pop unntil stk top lv+1 == i lv. ==pop until parent node become stk's top.
                     stk.pop()
-                stk[-1][1].right = i[1]     #stk top node.right = i node. else로 넘어와서 부모에 걸렸다는건 left가 무조건 차있다는 거니까 right.
+                stk[-1][1].right = i[1]     #stk top node.right = i node. bc it must be 2nd child.
                 stk.append(i)
-        return lvlist[0][1]                 #root node 반환.
+        return lvlist[0][1]                 #return root node.
 
             
             
